fd02c8bdde248f34de1c318607a4873a396da754
VVK Mulukutla <vvk.mulukutla@gmail.com>
2024-06-19 10:07:10 +0530

    cinbtacsafghds

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to contact page</h1>
 
 <p><a href="/">Click Here</a> to return </p>
-<p><a href="/about">Click Here</a> to go to about page </p>
+<p><a href="/about">Click Here</a> to go to about page </p>Y
